a4a3e0becb6de7dc59cedff2d1269198fd9bd0b4
TheBraveWhale <isavdlaan@hotmail.com>
2026-01-26 13:07:57 +0100

    updated the html to not display a city and temperature by default + added the JS for API getting temperature upon search and changing it on page

--- scr/script.js
+++ scr/script.js
@@ -1,9 +1,27 @@
-function changeCity(event) {
+function changeCurrentTemperature(response) {
+  let currentTemperature = document.querySelector(".current-temperature");
+  let searchTemperature = response.data.temperature.current;
+  let city = document.querySelector("h1");
+  city.innerHTML = response.data.city;
+
+  currentTemperature.innerHTML = Math.round(searchTemperature);
+}
+
+function searchCity(city) {
+  let apiKey = "fa25b40fcffa301t230o795c80b7e53f";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(changeCurrentTemperature);
+}
+
+function selectCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector(".city-search-input");
-  let city = document.querySelector("h1");
-  city.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("form");
-searchForm.addEventListener("submit", changeCity);
+searchForm.addEventListener("submit", selectCity);
+
+searchCity("Enschede");
